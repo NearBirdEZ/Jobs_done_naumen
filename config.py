ee34d9f7c572f2d558cf68617da554ee52f7398c
NearBirdEZ <40519198+NearBirdEZ@users.noreply.github.com>
2020-09-17 12:06:26 +0300

    '-'

--- config.py
+++
@@ -1,2 +0,0 @@
-log = input('Введите логин naumena\n')
-pas = input('Введите пароль naumena\n')
--- main.py
+++ main.py
@@ -1,87 +1,122 @@
-from selenium import webdriver
+import api_naumen
 import time
-from webdriver_manager.chrome import ChromeDriverManager
-from config import log, pas
-
-URL = "http://sdn.pilot.ru:8080/fx"
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-
-def start_naumen(url):
-    """Функция старта наумена"""
-    driver.get(url)
-    time.sleep(0.5)
-    driver.find_element_by_id('login').send_keys(log)
-    driver.find_element_by_id('password').send_keys(pas)
-    time.sleep(0.5)
-    driver.find_element_by_name('LogonFormSubmit').click()
-    time.sleep(1)
+import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
+from selenium.common.exceptions import NoSuchElementException
 
 
-def find_search(query):
-    """Функция поиска по заявке"""
-    index_url = "http://sdn.pilot.ru:8080/fx/sd/ru.naumen.sd.published_jsp?uuid=corebofs000080000ikhm8pnur5l85oc"
-    driver.get(index_url)
-    driver.find_element_by_id('sdsearch_ServiceCallIdSearchType').clear()
-    driver.find_element_by_id("sdsearch_ServiceCallIdSearchType").send_keys(query)
-    time.sleep(0.5)
-    driver.find_element_by_id("dosearchsdsearch_ServiceCallIdSearchType").click()
+class Send_message:
+    api = api_naumen.API_Naumen()
 
+    def __init__(self, gui_window):
+        gui_window.title('LDB T_T')
+        gui_window.geometry('320x600')
 
-def parser_body():
-    """Получение текста заявки"""
-    time.sleep(1)
-    descriprion = driver.find_element_by_class_name("servicecall_description_inner").text
-    return descriprion
+        self.lbl_l_naumen = tk.Label(gui_window, text='Login Naumen')
+        self.lbl_p_naumen = tk.Label(gui_window, text='Password Naumen')
 
+        self.login_area = tk.Entry(gui_window, width=30)
+        self.password_area = tk.Entry(gui_window, width=30)
 
-def send_mail(query, description):
-    """Функция для отправки почты. Передается номер и тело заявки
-    Получение ссылки на отправку комментария
-    """
+        self.btn_send = tk.Button(text='Отправить логин и\nпароль', command=self.send_log_pass)
+        self.btn_clear = tk.Button(text='Очистить поле\nлогина и пароля', command=self.clear)
 
-    link_mail = driver.find_element_by_id("ServiceCall.MailingList.SCMailing").get_attribute("href").split("?")[-1]
-    link_comment = driver.find_element_by_id("ServiceCall.SDCommentList.AddSDComment").get_attribute("href").split("?")[
-        -1]
-    driver.get("http://sdn.pilot.ru:8080/fx/$guic/ru.naumen.guic.components.forms.form_jsp?" + link_mail)
-    driver.find_element_by_name('mailText').clear()
-    text = f"""Здравствуйте!\nПросьба подтвердить выполенние работ ответным письмо по заявке № {query}\n\n{description}
-\n\nС уважением,\nСлужба Поддержки Пользователей\nООО \"Пилот - бизнес решения для торговли\"\n107023, Москва, 
-Барабанный переулок, дом 3\nтел. +7 495 564-8794\nфакс +7 495 564-8369\ne-mail: service_desk@pilot.ru"""
-    driver.find_element_by_id("mailText").send_keys(text)
-    time.sleep(2)
-    # клик по отправке
-    driver.find_element_by_name('send').click()
-    time.sleep(1)
+        self.btn_run = tk.Button(text='Запустить отправку писем', command=self.begin)
+        self.btn_view = tk.Button(text='Показать процесс\n в браузере',
+                                  command=self.view)
 
-    driver.get("http://sdn.pilot.ru:8080/fx/$guic/ru.naumen.guic.components.forms.form_jsp?" + link_comment)
-    driver.find_element_by_id("text").send_keys("Направлен запрос заказчику")
-    driver.find_element_by_id("privateComment").click()
-    driver.find_element_by_name('add').click()
+        self.txt = scrolledtext.ScrolledText(gui_window, width=30, height=24)
+        self.lbl_count = tk.Label(gui_window, text='')
 
+        self.lbl_l_naumen.place(relx=0.01, rely=0.01)
+        self.lbl_p_naumen.place(relx=0.01, rely=0.05)
 
-def main():
-    bad = []
-    count = 0
+        self.login_area.place(relx=0.37, rely=0.01)
+        self.password_area.place(relx=0.37, rely=0.05)
 
-    with open("query.txt", "r") as file:
-        for query in file:
-            find_search(query)
-            try:
-                send_mail(query, parser_body())
-            except:
-                bad.append(query)
-            time.sleep(1)
+        self.btn_send.place(relx=0.05, rely=0.11)
+        self.btn_clear.place(relx=0.6, rely=0.11)
+        self.btn_run.place(relx=0.27, rely=0.9)
+        self.btn_view.place(relx=0.3, rely=0.184)
+
+        self.txt.place(relx=0.1, rely=0.25)
+        self.lbl_count.place(relx=0.3, rely=0.95)
+
+    def view(self):
+        self.api.driver.set_window_position(0, 0)
+        self.btn_view.configure(state='disabled')
+
+    def send_log_pass(self):
+        login = self.login_area.get()
+        password = self.password_area.get()
+        if login == '' or password == '':
+            messagebox.showinfo('Ошибка', 'Не заполнены\nлогин и пароль')
+            self.clear()
+            return
+        self.clear()
+        self.btn_send.configure(state='disabled')
+        self.btn_clear.configure(state='disabled')
+        self.api.start_naumen(login, password)
+
+    def clear(self):
+        self.login_area.delete('0', tk.END)
+        self.password_area.delete('0', tk.END)
+
+    def run(self):
+        bad = []
+        lst = self.txt.get('0.1', tk.END).split('\n')
+        n = lst.count('')
+        count = 0
+        self.lbl_count.configure(text='Сейчас начнется выполнение')
+        for _ in range(n):
+            lst.remove('')
+
+        if len(lst) == 0:
+            messagebox.showinfo('Ошибка', 'Не найдена ни одна заявка')
+            return
+        total_request = len(lst)
+        self.btn_run.configure(state='disabled')
+        for request in lst:
             count += 1
-            print(count, "отправлено писем")
-    driver.close()
-    if len(bad) > 0:
-        with open('bad_request.txt', 'w') as f:
-            for request in bad:
-                f.write(request, '\n')
+            description = self.api.description_body(request)
+            text = f"""\
+Здравствуйте!
+Просьба подтвердить выполенние работ ответным письмо по заявке № {request}
+
+{description}
+
+С уважением,\nСлужба Поддержки Пользователей
+ООО \"Пилот - бизнес решения для торговли\"
+107023, Москва, Барабанный переулок, дом 3
+тел. +7 495 564-8794
+факс +7 495 564-8369
+e-mail: service_desk@pilot.ru
+"""
+            try:
+                self.api.send_mail(text)
+                self.api.send_comments(request, 'Направлен запрос для подтверждения выполнения работ', True)
+                time.sleep(1)
+            except NoSuchElementException:
+                bad.append(request)
+            self.lbl_count.configure(text=f'Выполнено {count} из {total_request}')
+            window.title(f'{count} из {total_request}')
+        if len(bad) > 0:
+            with open('errors.txt', 'w') as bad:
+                for query in bad:
+                    bad.write(query, '\n')
+        self.api.driver.close()
+        messagebox.showinfo('Done', 'Все было отправлено\n'
+                                    'Требуется проверить файл\n'
+                                    'errors.txt, если есть')
+        return
+
+    def begin(self):
+        threading.Thread(target=self.run, daemon=True).start()
 
 
 if __name__ == '__main__':
-    start_naumen(URL)
-    main()
+    window = tk.Tk()
+    app = Send_message(window)
+    window.mainloop()
